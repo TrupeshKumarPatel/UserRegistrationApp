af39da01cc2e94838e4c40d94d6b8a8eebe06ac4
Trupesh Patel <tr27p@uab.edu>
2019-03-26 13:36:20 -0500

    This contains database config file.

---
+++ instance/config.py
@@ -0,0 +1,6 @@
+# instance/config.py
+
+SECRET_KEY = 'p9Bv<3Eid9%$i01'
+#SQLALCHEMY_DATABASE_URI = 'mysql://dt_admin:dt2016@localhost/dreamteam_db'
+SQLALCHEMY_DATABASE_URI='sqlite:///app.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
